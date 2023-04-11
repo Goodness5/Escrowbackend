23f5c54ef1d0555661756bfd5c477fe3c31aa2a6
goodness5 <goodnesskolapo@gmail.com>
2023-04-11 22:17:00 +0100

    omo

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Iamsuperman:devxspace@Iamsuperman.mysql.pythonanywhere-services.com/Iamsuperman$devxspace'
 app.config['UPLOAD_FOLDER'] = 'uploads'
+app.secret_key = os.urandom(24)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -16,6 +18,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(42), nullable=False, unique=True)
     avatar = db.Column(db.String(20), nullable=True)
+    username = db.Column(db.String(20), nullable=True, unique=True)
 
     services = db.relationship('Service', backref='user', lazy=True)
 
@@ -28,40 +31,72 @@ class Service(db.Model):
     name = db.Column(db.String(20), nullable=False)
     image_file = db.Column(db.String(20), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    username = db.Column(db.Integer, db.ForeignKey('user.username'), nullable=False)
 
     def __repr__(self):
         return f"Name: {self.name}, Image: {self.image_file}"
 
 
+# @app.route('/login', methods=['POST'])
+# def login():
+#     address = request.headers.get('address')
+#     if not address:
+#         return jsonify({'error': 'Wallet address not provided.'}), 400
+
+#     # Set session cookie to the wallet address
+#     session['address'] = address
+
+#     # Check if user exists in the database, if not create new user
+#     user = User.query.filter_by(address=address).first()
+#     if not user:
+#         new_user = User(address=address)
+#         db.session.add(new_user)
+#         db.session.commit()
+
+#     return jsonify({'message': 'Logged in successfully.'})
+
+
+
 @app.route('/create_profile', methods=['POST'])
 def create_profile():
-    data = request.form
-    address = data.get('address')
-    avatar = request.files.get('avatar')
-
+    address = session.get('address')
     if not address:
-        return jsonify({'error': 'User address not provided.'}), 400
+        return jsonify({'error': 'User not logged in.'}), 401
 
-    if not avatar:
-        return jsonify({'error': 'Avatar file not provided.'}), 400
+    data = request.form
+    avatar = request.files.get('avatar')
+    username = data.get('username')
 
-    filename = secure_filename(avatar.filename)
-    avatar.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if not username:
+        return jsonify({'error': 'Username not provided.'}), 400
 
-    new_user = User(address=address, avatar=filename)
-    db.session.add(new_user)
+    filename = None
+    if avatar:
+        filename = secure_filename(avatar.filename)
+        avatar.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    user = User.query.filter_by(address=address).first()
+    if not user:
+        user = User(address=address)
+
+    user.username = username
+    if filename:
+        user.avatar = filename
+
+    db.session.add(user)
     db.session.commit()
+
     return jsonify({'message': 'Profile created successfully.'})
 
 
 @app.route('/update_profile', methods=['PUT'])
 def update_profile():
-    data = request.form
-    address = data.get('address')
-    avatar = request.files.get('avatar')
+    if 'address' not in session:
+        return jsonify({'error': 'User not logged in.'}), 401
 
-    if not address:
-        return jsonify({'error': 'User address not provided.'}), 400
+    data = request.form
+    address = session['address']
+    avatar = request.files.get('avatar')
 
     user = User.query.filter_by(address=address).first()
     if not user:
@@ -78,27 +113,25 @@ def update_profile():
 
 @app.route('/register_service', methods=['POST'])
 def register_service():
+    if 'address' not in session:
+        return jsonify({'error': 'User not logged in.'}), 401
+
     data = request.form
-    user_id = data.get('user_id')
+    address = session['address']
     service_name = data.get('name')
-    service_image = request.files.get('image')
-
-    if not user_id:
-        return jsonify({'error': 'User ID not provided.'}), 400
-
-    if not service_name:
-        return jsonify({'error': 'Service name not provided.'}), 400
-
+    service_image = request.files.get('image_file')
     if not service_image:
         return jsonify({'error': 'Image file not provided.'}), 400
 
     filename = secure_filename(service_image.filename)
     service_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+    user_id = User.query.filter_by(address=address).first().id
     new_service = Service(name=service_name, image_file=filename, user_id=user_id)
     db.session.add(new_service)
     db.session.commit()
     return jsonify({'message': 'Service registered successfully.'})
+
 
 
 @app.route('/list_services', methods=['GET'])
